2361769845da9bc80a3311e9f9768fc304b04af1
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-02-26 11:39:47 +0000

    Removing unused files and tidying directory structure.

--- src/start.sh
+++ src/start.sh
@@ -6,4 +6,4 @@
 # it saves you a LOT of resources avoiding full-desktops envs
 
 rm /tmp/.X0-lock &>/dev/null || true
-startx /usr/src/app/node_modules/electron/dist/electron -r babel-register /usr/src/app/src/client/main.js --enable-logging
+startx /usr/src/app/node_modules/electron/dist/electron -r babel-register /usr/src/app/src/main.js --enable-logging
